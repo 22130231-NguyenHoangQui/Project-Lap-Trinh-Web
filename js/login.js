fa95b5d0d4998159fa61e40917e92622fc41d066
22130231_NguyenHoangQui <22130231@st.hcmuaf.edu.vn>
2024-10-18 16:03:19 +0700

    create-login-forgotPassword

---
+++ js/login.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn không cho biểu mẫu được gửi
+
+    // Lấy giá trị từ các trường nhập liệu
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Kiểm tra thông tin đăng nhập
+    if (username !== "Qui" || password !== "123") {
+      // Hiển thị thông báo lỗi nếu thông tin không chính xác
+      document.getElementById("error-message").style.display = "block";
+    } else {
+      // Thực hiện hành động đăng nhập thành công 
+      alert("Đăng nhập thành công!");
+      window.location.href = "../index.html"; 
+    }
+  });
